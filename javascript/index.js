d1d3683bada80c2ad5dcdf33f033b23c656a0934
Luca Di Vittorio <luca.div@hotmail.com>
2022-10-10 16:07:50 +0100

    made the seconds move correctly

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,16 @@
 // console.log('Hello from index.js');
+function setTime() {
+  const time = new Date();
+  const hours = document.querySelector('.hours')
+  const minutes = document.querySelector('.minutes')
+  const seconds = document.querySelector('.seconds')
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
+  seconds.setAttribute('style', `transform: rotate(${(second + 15) / 60 * 360}deg)`)
+}
+
+setInterval(setTime, 1000);
